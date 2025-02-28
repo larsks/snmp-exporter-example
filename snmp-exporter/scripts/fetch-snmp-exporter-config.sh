fcc84038947b2de141a933bc2cf73b6763a5e3a2
Lars Kellogg-Stedman <lars@oddbit.com>
2025-02-28 13:21:32 -0500

    Use ocp-on-nerc generator image

--- snmp-exporter/scripts/fetch-snmp-exporter-config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-: ${SNMP_EXPORTER_CONFIG_URL:=https://raw.githubusercontent.com/larsks/snmp-exporter-example-config/refs/heads/main/snmp.yml}
-
-set -x
-curl -SsfL -o /config/snmp.yml "$SNMP_EXPORTER_CONFIG_URL"
